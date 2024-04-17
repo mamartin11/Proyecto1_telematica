f57a93a0cefedb09a399746c862b0d897c7170f0
mamartin11 <111911651+mamartin11@users.noreply.github.com>
2024-04-17 18:56:20 -0500

    Update server.c

--- server.c
+++ server.c
@@ -23,7 +23,7 @@ int main(int argc, char **argv) {
         exit(0);
     }
 
-    char *ip = "127.0.0.1";
+    char *ip = "172.20.10.2";
     int port = atoi(argv[1]);
 
     struct DNS_Record dns_records[MAX_RECORDS] = {
